f4261d8e0780cba4569bbcfe8c768a9664a96a03
AD17102 <alexdikovipad@gmail.com>
2024-05-28 15:56:00 +0200

    Need to rewrite the connect_to_ESP32 function.

--- app/src/main/java/com/example/pint_gui/MainActivity.kt
+++ app/src/main/java/com/example/pint_gui/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.example.pint_gui
 
 import android.annotation.SuppressLint
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pint_gui.ui.theme.PINT_GUITheme
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.File
@@ -30,13 +25,13 @@ import java.net.Socket
 import kotlin.system.measureTimeMillis
 
 
-var timeout_value = 15000;
-var ESP32_port = 80;
-var ESP32_IP = "192.168.1.1" //TODO: change this to the actual ip address.
-var floadValues : List<Float> = emptyList();
+var timeout_value = 15000
+var ESP32_port = 80
+var ESP32_IP = "192.168.32.41"
+var floadValues : List<Float> = emptyList()
 var statusval : String = ""
 class MainActivity : ComponentActivity() {
-    private var connectionJob: Job? = null;
+    private var connectionJob: Job? = null
      val dataList = mutableListOf<Float>()
     @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +39,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             //controlling between the main screen and the history screen
-            val navController = rememberNavController();
+            val navController = rememberNavController()
 
             var connectionJob = CoroutineScope(Dispatchers.Default).launch{
                 while(isActive){
@@ -55,7 +50,7 @@ class MainActivity : ComponentActivity() {
                 break;
 
                 }
-                delay(timeMillis = measureTimeMillis { timeout_value }); //15 seconds
+                delay(timeMillis = measureTimeMillis { timeout_value }) //15 seconds
             }
 
             PINT_GUITheme {
@@ -84,20 +79,20 @@ class MainActivity : ComponentActivity() {
 
  fun connect_to_ESP32(): Boolean{
         return try{
-            val socket = ServerSocket(ESP32_port);
-            val client: Socket = socket.accept();
-            val reader = BufferedReader(InputStreamReader(client.getInputStream()));// Blocking call, waits for ESP32 connection
-            val message = reader.readLine();
-            val values = message.split(',').map{it.toFloat()}
-            floadValues = values.take(6).map { it.toFloat() }
-            statusval = values[6].toString();
-            store_sen_data(floadValues, statusval);
-            return true;
+            //TODO: Make a sketch that should work on an arduino as well as an esp32 and test it using this application
+            val socket = ServerSocket(ESP32_port)
+            //val client: Socket = socket.accept()
+            //This line breaks shit, so beweare of it
+            //val reader = BufferedReader(InputStreamReader(client.getInputStream()))// Blocking call, waits for ESP32 connection
+            //val message = reader.readLine()
+            //val values = message.split(',').map{it.toFloat()}
+           // floadValues = values.take(6).map { it.toFloat() }
+            //statusval = values[6].toString()
+            //store_sen_data(floadValues, statusval)
+            return true
         }catch(e : Exception){
             e.printStackTrace()
-            //TODO: replace this with false
-            return true;
-
+            return false
         }
 
 }
@@ -108,7 +103,7 @@ try{
     file.appendText("${floatValue.joinToString(",")}, $stringValue\n")
 }
 catch(e: Exception){
-    file.appendText("Failed to read data ");
+    file.appendText("Failed to read data ")
 
 }
 }
--- app/src/main/java/com/example/pint_gui/mainscreen.kt
+++ app/src/main/java/com/example/pint_gui/mainscreen.kt
@@ -1,25 +1,19 @@
 package com.example.pint_gui
 
 import android.content.Context
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionContext
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -27,8 +21,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 import java.io.InputStream
 import java.net.HttpURLConnection
